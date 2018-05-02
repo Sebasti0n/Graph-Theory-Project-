30b184dcc189a7b5d17e0d2628c2b793c1b5129b
Sebastian <38714137+Sebasti0n@users.noreply.github.com>
2018-05-02 12:51:27 -0500

    Update MST.py

--- Graph Theory Project/MST.py
+++ Graph Theory Project/MST.py
@@ -1,5 +1,16 @@
+""" This file should be your main program which solves the MST using Prims"""
 
 from Prims import Prims
+from Weighted_Graph import* 
+
+MST = (VT, ET)
 
 
-def MST(Textfile, algorithm = 'Prims', initial_vertex = 0, show = False):
+def MST(textfile, algorithm = 'Prims', initial_vertex = 0, show = False):
+    
+    return Prims
+
+
+def initial_tree(initial_vertex):
+    return({initial_vertex}, [])
+print(initial_tree(0))
