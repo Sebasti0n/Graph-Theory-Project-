e5c74134fd036a2655b10e9eab19c2efb06413ff
Sebastian <38714137+Sebasti0n@users.noreply.github.com>
2018-05-02 17:45:05 -0500

    Update MST.py
    MST (Minimum Spanning Tree) calls all the function that works out the Tree graph with optimal cost

--- Graph Theory Project/MST.py
+++ Graph Theory Project/MST.py
@@ -1,16 +1,14 @@
 """ This file should be your main program which solves the MST using Prims"""
 
+
 from Prims import Prims
-from Weighted_Graph import* 
-
-MST = (VT, ET)
-
 
 def MST(textfile, algorithm = 'Prims', initial_vertex = 0, show = False):
     
-    return Prims
+    Prims(G)
 
-
-def initial_tree(initial_vertex):
+"""
+def initial_tree(initial_vertex): #This is a placeholder for the algorithm that calls initial_tree
     return({initial_vertex}, [])
 print(initial_tree(0))
+"""
