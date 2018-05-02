a1b04046c41e96369480c1a1c48ae923acf62a9a
Sebastian <38714137+Sebasti0n@users.noreply.github.com>
2018-05-02 12:47:20 -0500

    Update prims_functions.py
    Optional function that I came up with to test if it runs on your system

--- Graph Theory Project/prims_functions.py
+++ Graph Theory Project/prims_functions.py
@@ -1,57 +1,62 @@
-from Weighted_Graph import* 
-G = Weighted_Graph('test.txt')
+"""
+This file should store all the functions that you write which will be needed
+in the steps of Prim's algorithm"
+"""
+
+from Weighted_Graph import*
+G = Weighted_Graph("test.txt")
 
 G.draw_graph()
 
-def cost(G, e):
-    return G.edge_dict()[e]
-#print("cost function at (0,1)", cost(G, (0,1)))
-    
-def initial_tree(initial_vertex):
-    return ({initial_vertex}, [])
-print(initial_tree(3))
+#Variables for the function
+V = G.vertex_set()      #Vertecies
+E = G.edge_set()        #Edges
+cost = G.edge_dict()    #weight
+VT = {0}
+ET = []
 
-def incident_edges(G, T):
+def incident_edges(VT, ET):     
     
     edges = []
-    for e in G.edge_set():
-        for V in T[0]:
-            if V in e: 
+    for v in VT:
+        for e in E:
+            if v in e and e not in ET:
                 edges.append(e)
-    
-    return [e for e in edges if e not in T[1]]
-
-print(incident_edges(G, initial_tree(0)))
-
-def valid_incident_edges(G, T):
-    edges = []
-    for e in incident_edges(G,T):
-        if e[0] not in T[0] or e[1] not in T[0]:
-            edges.append(e)
+                
+    E_valid = []
+    for e in edges:
+        if e[0] not in VT or e[1] not in VT:
+            E_valid.append(e)
             
-    return edges
+    return E_valid
 
-print(valid_incident_edges(G, T))
+print(incident_edges(VT, ET))
 
-def min_valid_incident_edge(G, T):
-    valid_edges = valid_incident_edges(G, T)
-    min_edge = valid_edges[0]
-    
-    for e in valid_edges: 
-        if cost(e) < cost(min_edge):
-            min_edge = e
+def min_incident_edges(incident_edges):
+    min_cost_edge = incident_edges[0]
+    for i in range(len(incident_edges)):
+        if cost[incident_edges[i]] < cost[min_cost_edge]:
+            min_cost_edge = incident_edges[i]
             
-    return min_edge
+    return min_cost_edge
+possible_edges = incident_edges(VT, ET)
 
-print(min_valid_incident_edge(G,T))
+print(min_incident_edges(possible_edges))
+
+def add_valid_edge(VT, ET): #test to see if the graph plots
+    incident_valid = incident_edges(VT, ET)
+    min_edge = min_incident_edges(incident_valid)
+    if min_edge[0] not in VT:
+        VT.add(min_edge[0])
+    if min_edge[1] not in VT:
+        VT.add(min_edge[1])
+    ET.append(min_edge)
+    print('Result: ',VT,ET)
+    return cost[min_edge]
+
+total = 0
+while VT != V:
+    current = add_valid_edge(VT,ET)
+    total = total + current
+    print('Total :', total)
     
-    
-#do not add edges that make a cycle in the tree function 
-
-"""
-Want (v,W)
-
-if V and W not in V(T)
-7(V e V(T) and W e V(T))
-
-"""
