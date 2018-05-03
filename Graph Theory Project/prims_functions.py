d5ed1a086a8c69d51189ceb2bbcabc65a332bcd1
Sebastian <38714137+Sebasti0n@users.noreply.github.com>
2018-05-02 22:14:22 -0500

    Update prims_functions.py
    Module that stores the functions that we will use to create Prim's algorithm. 
    
    do not add edges that make a cycle in the tree function 
    
    Want (v,W)
    if V and W not in V(T)
    7(V e V(T) and W e V(T))

--- Graph Theory Project/prims_functions.py
+++ Graph Theory Project/prims_functions.py
@@ -1,18 +1,21 @@
-from Weighted_Graph import*
+"""This file should store all the functions taht you write which will be needed 
+ in the steps of Prim's algorithm"""
  
+from Weighted_Graph import*
+                                 #This is importing the weighted graph
 G = Weighted_Graph('test.txt')
 G.draw_graph()
 
 
 def cost(G, e): #Determines the cost for Edges
     return G.edge_dict()[e]
-#print("cost function at (0,1)", cost(G, (0,1)))
+#print("cost function at (0,1)", cost(G, (0,1))) -- Example 
     
-def initial_tree(initial_vertex):
+def initial_tree(initial_vertex): #This will pick where the inital vertex is
     return ({initial_vertex}, [])
 
 
-def incident_edges(G, T):
+def incident_edges(G, T): #This finds any incident edges
     
     edges = []
     for e in G.edge_set():
@@ -22,7 +25,7 @@ def incident_edges(G, T):
     
     return [e for e in edges if e not in T[1]]
 
-def valid_incident_edges(G, T):
+def valid_incident_edges(G, T): #This returns out the valid incident edges
     edges = []
     for e in incident_edges(G,T):
         if e[0] not in T[0] or e[1] not in T[0]:
@@ -30,7 +33,7 @@ def valid_incident_edges(G, T):
             
     return edges
 
-def min_valid_incident_edge(G, T):
+def min_valid_incident_edge(G, T):  #This calculates the minimum cost     
     valid_edges = valid_incident_edges(G, T)
     min_edge = valid_edges[0]
     
@@ -39,6 +42,7 @@ def min_valid_incident_edge(G, T):
             min_edge = e
             
     return min_edge
+
 
     
 #do not add edges that make a cycle in the tree function 
