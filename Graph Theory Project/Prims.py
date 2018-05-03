c13dde495d0e620cb6c4d24e58fe57a8da5fb75a
sbsupramike <38796222+sbsupramike@users.noreply.github.com>
2018-05-02 21:36:14 -0500

    UpdatePrims.py
     #michaels attempt for Prims.py
    
    from prims_fucntions import*
    
    def Prims(textfile):
        
        total = 0
        A = []
        n = len(Graph)
        A = [[None for x in range(0, n)] for y in range(1, 4)]
       
        for x in range(1, n):
            A[0][x] = 'Y'
            A[1][x] = 0
            A[2][x] = 0
    
        for neighbour in Graph[1]: 
            A[1][neighbour-1] = 1
            A[2][neighbour-1] = Graph[1][neighbour]
            
        current = 1
        T = [current]
        MST_edges = {}
        count = 0
        while len(T) < n:
            x = search_min(current, A)
            T.append(x)
            MST_edges[x] = A[1][x]
            A[0][x] = 'N'
            total += A[2][x]
            
            for neighbour in Graph[x]:
                if A[0][neighbour-1] != 'N':
                    if Graph[x][neighbour] < A[2][neighbour-1]:
                        A[1][neighbour-1] = x
                        A[2][neighbour-1] = Graph[x][neighbour]
            count += 1
            current = T[count]
        return ('Your MST is', total)
    def search_min(current, A):
        minimum_cost = 100
        minimum_vertex = 1
        for x in range(1,len(A[0])):
            if A[1][x] != None and A[0][x] != 'N' and A[2][x] < minimum_cost:
                    minimum_cost = A[2][x]
                    minimum_vertex = x
                    return minimum_vertex

--- Graph Theory Project/Prims.py
+++ Graph Theory Project/Prims.py
@@ -31,6 +31,54 @@ print('Result: ',VT,ET)
 G.draw_subgraph(Tree)
 #return cost[min_edge]
 
+ #michaels attempt for Prims.py
+
+from prims_fucntions import*
+
+def Prims(textfile):
+    
+    total = 0
+    A = []
+    n = len(Graph)
+    A = [[None for x in range(0, n)] for y in range(1, 4)]
+   
+    for x in range(1, n):
+        A[0][x] = 'Y'
+        A[1][x] = 0
+        A[2][x] = 0
+
+    for neighbour in Graph[1]: 
+        A[1][neighbour-1] = 1
+        A[2][neighbour-1] = Graph[1][neighbour]
+        
+    current = 1
+    T = [current]
+    MST_edges = {}
+    count = 0
+    while len(T) < n:
+        x = search_min(current, A)
+        T.append(x)
+        MST_edges[x] = A[1][x]
+        A[0][x] = 'N'
+        total += A[2][x]
+        
+        for neighbour in Graph[x]:
+            if A[0][neighbour-1] != 'N':
+                if Graph[x][neighbour] < A[2][neighbour-1]:
+                    A[1][neighbour-1] = x
+                    A[2][neighbour-1] = Graph[x][neighbour]
+        count += 1
+        current = T[count]
+    return ('Your MST is', total)
+def search_min(current, A):
+    minimum_cost = 100
+    minimum_vertex = 1
+    for x in range(1,len(A[0])):
+        if A[1][x] != None and A[0][x] != 'N' and A[2][x] < minimum_cost:
+                minimum_cost = A[2][x]
+                minimum_vertex = x
+                return minimum_vertex
+            
 
     
     
